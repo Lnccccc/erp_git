c2d2eac24b85db3c44b6d7f9150c7bb6888ac50e
lnccccc <601452185@qq.com>
2018-05-08 12:07:13 +0800

    修改： 1、仅显示最新日期的数据 2、新增主图、标题及价格数据 3、导航栏修改

--- keyword_spider.py
+++ keyword_spider.py
@@ -34,7 +34,7 @@ class Keyword(object):
         self.front = 'https://www.aliexpress.com/wholesale?initiative_id=SB_20180412174003&site=glo&g=y&SearchText='
         self.behind = '&needQuery=n&page='
 
-    def get_keyword(self, filename='运动防护health.xls'):
+    def get_keyword(self, filename='keyword_list_health.xls'):
         keyword_list = []
         filename = filename
         dataframe = pd.read_excel(filename)
