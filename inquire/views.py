ca9c72afd25efa793f4257e6f1cbd2f108a80372
lnccccc <601452185@qq.com>
2018-05-09 20:41:36 +0800

    修改： 1、仅显示最新日期的数据 2、新增主图、标题及价格数据 3、导航栏修改

--- inquire/views.py
+++ inquire/views.py
@@ -37,7 +37,7 @@ def Getdata(request):
     except:
         wrong_url = '您输入的商品链接有误，请输入正确的速卖通商品链接'
         return render(request,'detail.html',context={'wrong_url':wrong_url})
-    results = keyword3.objects.filter(urlid=urlid).order_by('-keyword')
+    results = keyword3.objects.filter(urlid=urlid).order_by('-keyword')[:10]
     for i in results:
         if i.updatatime.strftime("%y-%m-%d") not in date_results:
             date_results.append(i.updatatime.strftime("%y-%m-%d"))
