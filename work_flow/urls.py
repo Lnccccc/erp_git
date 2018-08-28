9358d75158db92bfdda521ff02a5a042eb41ca23
lnccccc <601452185@qq.com>
2018-08-28 18:05:58 +0800

    解决订单状态筛选功能

--- work_flow/urls.py
+++ work_flow/urls.py
@@ -4,5 +4,5 @@ app_name = 'work_flow'
 urlpatterns = [
     path("",views.index,name="index"),
     path("add_order/",views.add_order,name='add_order'),
-
+    path("status_<int:status_cd>/",views.status,name='status')
 ]
--- work_flow/views.py
+++ work_flow/views.py
@@ -3,6 +3,8 @@ from .models import orders_list,order_stat
 from uuid import uuid1
 from django.db.models import Count
 # Create your views here.
+
+
 def add_order(request):
     results = orders_list.objects.raw('select a.*,b.stat_nam from work_flow_orders_list a left join work_flow_order_stat b on a.order_status = b.stat_cd')
     order_status = order_stat.objects.all()
@@ -35,8 +37,9 @@ def add_order(request):
                      order_num=_order_num,order_detail=_order_detail,
                      ps=_ps,order_status=2,person_incharge=_person_incharge)
     ol.save()
-
     return render(request,'order_list.html',context={"results":results,"count":tmp_list})
+
+
 def index(request):
     tmp_list = []
     results = orders_list.objects.raw('select a.*,b.stat_nam from work_flow_orders_list a left join work_flow_order_stat b on a.order_status = b.stat_cd')
@@ -54,7 +57,27 @@ def index(request):
     tmp_list.append(stat_5)
     tmp_list.append(stat_6)
     tmp_list.append(stat_7)
-
-
-
     return render(request,'order_list.html',context={"results":results,"count":tmp_list})
+
+def status(request,status_cd):
+    tmp_list = []
+    stat_1 = orders_list.objects.filter(order_status='1').aggregate(count_1=Count('order_status')).get('count_1')
+    tmp_list.append(stat_1)
+    stat_2 = orders_list.objects.filter(order_status='2').aggregate(count_1=Count('order_status')).get('count_1')
+    tmp_list.append(stat_2)
+    stat_3 = orders_list.objects.filter(order_status='3').aggregate(count_1=Count('order_status')).get('count_1')
+    tmp_list.append(stat_3)
+    stat_4 = orders_list.objects.filter(order_status='4').aggregate(count_1=Count('order_status')).get('count_1')
+    tmp_list.append(stat_4)
+    stat_5 = orders_list.objects.filter(order_status='5').aggregate(count_1=Count('order_status')).get('count_1')
+    stat_6 = orders_list.objects.filter(order_status='6').aggregate(count_1=Count('order_status')).get('count_1')
+    stat_7 = orders_list.objects.filter(order_status='7').aggregate(count_1=Count('order_status')).get('count_1')
+    tmp_list.append(stat_5)
+    tmp_list.append(stat_6)
+    tmp_list.append(stat_7)
+    if status_cd:
+        results = orders_list.objects.raw('select a.*,b.stat_nam from work_flow_orders_list a left join work_flow_order_stat b on a.order_status = b.stat_cd where a.order_status=%d' % status_cd)
+    elif status_cd == 0:
+        results = orders_list.objects.raw('select a.*,b.stat_nam from work_flow_orders_list a left join work_flow_order_stat b on a.order_status = b.stat_cd')
+    return render(request,'order_list.html',context={"results":results,"count":tmp_list})
+
